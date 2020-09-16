3501a83191836727e1e3938fe6c398b446e6c584
Watcharapon Wanlanon <610108020009@dpu.ac.th>
2020-09-16 15:27:42 +0700

    food

---
+++ food/data.js
@@ -0,0 +1,60 @@
+const data = [
+    {
+        id:1,
+        name:'KFC (เคเอฟซี) - แม็คโคร จรัญสนิทวงศ์ 37',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST00000799/hero/7dc1e8b39ff84f16abc5b31314324363_1600007790278417289.jpeg',
+        type:'Partner,Fast Food,Fried Chicken'
+    },
+    {
+        id:2,
+        name:'McDonalds (แมคโดนัลด์) - พาต้า ปิ่นเกล้า',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000004bl/hero/5c2f639864be46968813496061ab209c_1600008272596123546.jpeg',
+        type:'Partner,Burgers,Fast Food,Fried Chicken'
+    },
+
+    {
+        id:3,
+        name:'ลาบเป็ดอุบล - บางขุนนนท์',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000008et/hero/db11b40b2782422b868fea5da7ec154d_1584504458628563773.jpeg',
+        type:'Coupon,Partner,Esan'
+    },
+
+    {
+        id:4,
+        name:'Cafe Amazon (คาเฟ่ อเมซอน) - ถนนมหรรณพ',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTEUGBXT2D2TJ/hero/776901be512444519763199d5fe9a272_1593408148178779119.jpeg',
+        type:'Partner,Coffee & Tea'
+    },
+
+    {
+        id:5,
+        name:'Pizza Hut (พิซซ่าฮัท) - ถนนบำรุงเมือง',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CYTDJJE1VY2BEJ/hero/1d6f0d857f8e45288f5f646ba3817bb7_1598195166217373044.jpg',
+        type:'Coupon,Partner,Pizza'
+    },
+    {
+        id:6,
+        name:'ข้าวมันไก่ไหหลำนายจิว - ถนนดินสอ',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZE1VN5KVEVEGX/hero/5819f2072fed4be9a7a4ccbbc2198626_1590131373308341816.jpeg',
+        type:'Partner,Cooked to Order'
+    },
+    {
+        id:7,
+        name:'อ้อย ข้าวคลุกกะปิ - อรุณ​อัม​รินทร์',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/3-CZKTN2TYCTCEFA/hero/cb5b82ec7edb425daeabe9adf1fea062_1593683118171424592.jpeg',
+        type:'Partner,Cooked to Order'
+    },
+    {
+        id:8,
+        name:'จิวโจ๊กบางลำพู สุกี้กะทะร้อน - ถนนตานี',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zo/hero/upload-photo-Hero_Photo_cb41e8148e50410a8f33632e0c336c02_1552533003406029094.jpeg',
+        type:'Partner,Noodles'
+    },
+    {
+        id:9,
+        name:'After You Dessert Cafe (อาฟเตอร์ยู) - ท่ามหาราช',
+        imageURL:'https://d1sag4ddilekf6.cloudfront.net/compressed/merchants/THGFIST000001zd/hero/0ce91745497846f784dcfc52f0811ecb_1594618070442972434.jpeg',
+        type:'Partner,Bubble Tea'
+    }
+    ];
+    module.exports = data;
